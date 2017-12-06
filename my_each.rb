a35f2f95dd300d39c124a6ebee8480d3ac1a6a73
Andy Maskiell <maskiella@gmail.com>
2017-12-06 22:23:56 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
 def my_each(array)
   # code here
+  if block_given?
+    counter = 0
+    while counter < array.length
+      yield array[counter]
+      counter += 1
+    end
+    array
+  end
 end
